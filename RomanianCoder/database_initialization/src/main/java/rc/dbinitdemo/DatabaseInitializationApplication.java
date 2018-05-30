73d8aa243363bc47706e30383ff50d287f8f13fc
ninpo99 <ninpo99@hotmail.com>
2018-05-29 22:52:13 -0500

    Added database initialization project

---
+++ RomanianCoder/database_initialization/src/main/java/rc/dbinitdemo/DatabaseInitializationApplication.java
@@ -0,0 +1,12 @@
+package rc.dbinitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseInitializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseInitializationApplication.class, args);
+	}
+}
